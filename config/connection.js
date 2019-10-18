f89fc65b46e460c6427edb91a345ebbd787b22da
Kristen Stroup <kristenwhitson@gmail.com>
2019-10-17 21:53:14 -0500

    set up for jawsdb

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,16 @@
 var mysql = require("mysql");
 require("dotenv").config();
 
-var connection = mysql.createConnection({
-    host: process.env.DB_HOST || "localhost",
-    port: process.env.DB_PORT || 3306,
-    user: process.env.DB_USERNAME || "root",
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE || "burgers_db"
-});
+var connection = process.env.JAWSDB_URL ?
+    mysql.createConnection(process.env.JAWSDB_URL)
+    :
+    mysql.createConnection({
+        host: process.env.DB_HOST || "localhost",
+        port: process.env.DB_PORT || 3306,
+        user: process.env.DB_USERNAME || "root",
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE || "burgers_db"
+    });
 
 // Make connection.
 connection.connect(function (err) {
